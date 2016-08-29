dbac98c288524142883f6474ca0d5d371c7df0a1
Chris Mahr <mahrtian@gmail.com>
2016-08-29 12:29:08 -0400

    added overlay image show and hide functions

--- main.js
+++ main.js
@@ -7,11 +7,21 @@
     var apiKey = "517ed29cb6d385f02ccb1f513ec4215e";
     var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=" +$(".pure-input-rounded").val()+ "&api_key=" +apiKey+ "&per_page=10&format=json&jsoncallback=?";
 
-    $.getJSON(url, function(data) {
+    $.getJSON(url, function(data){
       $.each(data.photos.photo, function(i, item){
         var src = "http://farm" +item.farm+ ".static.flickr.com/" +item.server+ "/" +item.id+ "_" +item.secret+ "_m.jpg";
-        $("<img/>").attr("src", src).appendTo("#images");
+        $("<img/>").attr("src", src).click(function(){
+          $("#overlay-image").attr("src", src);
+          $(".overlay").show();
+          $(".overlay-content").show();
+        }).appendTo("#images");
       });
     });
+  })
+
+  $(".overlay-content").click(function(){
+    $(".overlay-image").attr("src", "");
+    $(".overlay").hide();
+    $(".overlay-content").hide();
   })
 })();
