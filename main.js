deb33a1077bc52808b07529e7df1610f42e17edd
Chris Mahr <mahrtian@gmail.com>
2016-08-29 10:54:20 -0400

    rendered first set of 10 photos onto page

--- main.js
+++ main.js
@@ -8,16 +8,10 @@
     var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=" +$(".pure-input-rounded").val()+ "&api_key=" +apiKey+ "&per_page=10&format=json&jsoncallback=?";
 
     $.getJSON(url, function(data) {
-      console.log(data.photos.photo[0]);
+      $.each(data.photos.photo, function(i, item){
+        var src = "http://farm" +item.farm+ ".static.flickr.com/" +item.server+ "/" +item.id+ "_" +item.secret+ "_m.jpg";
+        $("<img/>").attr("src", src).appendTo("#images");
+      });
     });
-
-    // $.ajax({
-    //   type: "GET",
-    //   url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=soccer&api_key=517ed29cb6d385f02ccb1f513ec4215e&per_page=10&format=json&jsoncallback=?",
-    //   success: function(data){
-    //     var photos = data.photos;
-    //     console.log(photos.photo[0]);
-    //   }
-    // });
   })
 })();
