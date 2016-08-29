74f1e8a8ea56ea5a6253241c61e6fcfe39e56fe8
Chris Mahr <mahrtian@gmail.com>
2016-08-29 13:31:23 -0400

    added next button pagination functionality

--- main.js
+++ main.js
@@ -5,9 +5,9 @@
     event.preventDefault();
 
     var apiKey = "517ed29cb6d385f02ccb1f513ec4215e";
-    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=" +$(".pure-input-rounded").val()+ "&api_key=" +apiKey+ "&per_page=10&format=json&jsoncallback=?";
-
-    $.getJSON(url, function(data){
+    var pageNumber = 1;
+    
+    $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=" +$(".pure-input-rounded").val()+ "&api_key=" +apiKey+ "&page=" +pageNumber+ "&per_page=10&format=json&jsoncallback=?", function(data){
       $.each(data.photos.photo, function(i, item){
         var src = "http://farm" +item.farm+ ".static.flickr.com/" +item.server+ "/" +item.id+ "_" +item.secret+ "_m.jpg";
         $("<img/>").attr("src", src).click(function(){
@@ -17,9 +17,32 @@
         }).appendTo("#images");
       });
     });
+
+    $("<button class='previous-button'>Previous Page</button>").appendTo("#toggle-buttons").hide();
+    $("<button class='next-button'>Next Page</button>").appendTo("#toggle-buttons");
+    
+    $(".next-button").click(function(){
+      $("#images").empty();
+      pageNumber++;
+      $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=" +$(".pure-input-rounded").val()+ "&api_key=" +apiKey+ "&page=" +pageNumber+ "&per_page=10&format=json&jsoncallback=?", function(data){
+        $.each(data.photos.photo, function(i, item){
+          var src = "http://farm" +item.farm+ ".static.flickr.com/" +item.server+ "/" +item.id+ "_" +item.secret+ "_m.jpg";
+          $("<img/>").attr("src", src).click(function(){
+            $("#overlay-image").attr("src", src);
+            $(".overlay").show();
+            $(".overlay-content").show();
+          }).appendTo("#images");
+        });
+      });
+      if (pageNumber > 1) {
+        $(".previous-button").show();
+      } else if (pageNumber === data.photos.pages) {
+        $(".next-button").hide();
+      }  
+    });      
   })
 
-  $(".overlay-content").click(function(){
+  $(".overlay").click(function(){
     $(".overlay-image").attr("src", "");
     $(".overlay").hide();
     $(".overlay-content").hide();
